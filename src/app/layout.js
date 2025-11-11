c84e201ad113dd4f1b9b3be14323c99b07dfa593
vishal.dhanotiya2@gmail.com <vishaldhanotiya@Vishals-MacBook-Air.local>
2025-11-11 18:36:42 +0530

    Added google analytic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+          <GoogleAnalytics gaId="G-24ZQVM678J" />
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
