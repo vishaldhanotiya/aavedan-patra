a3a015f78bc5d3e2e69d5f0deb184a6cc88b9f7f
Vishal Dhanotiya <vishal.dhanotiya2@gmail.com>
2025-11-27 01:01:28 +0530

    added sitemap.xml

---
+++ src/app/sitemap.js
@@ -0,0 +1,48 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://aavedanpatra.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/applications",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/applications/job",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/applications/job-application-for-teacher",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/faq",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/support",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/privacy-policy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/terms-of-service",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/disclaimer",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://aavedanpatra.in/contact-us",
+      lastModified: new Date(),
+    },
+  ];
+}
