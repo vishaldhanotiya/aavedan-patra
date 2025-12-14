10ad6527cfef2e6f0e45577ced9ccb3e78124390
Vishal Dhanotiya <vishal.dhanotiya2@gmail.com>
2025-12-14 14:22:04 +0530

    fix build error

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,176 +2,175 @@ export default function sitemap() {
   return [
     {
       url: "https://aavedanpatra.in",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/job",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-        {
+    {
       url: "https://aavedanpatra.in/applications/application-for-bank-job",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-bonafide-certificate-request",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-business-loan",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-car-loan",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-caste-certificate-request",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-casual-leave",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-character-certificate-request",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-college-admission",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-course-admission",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-education-loan",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-emergency-loan",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-experience-certificate-request",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-government-scholarship",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-home-loan",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-income-certificate",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-manager-position-job",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-merit-scholarship",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-need-based-scholarship",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-office-assistant-job",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-personal-leave",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-personal-loan",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-private-scholarship",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-readmission",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-research-grant",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-school-admission",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-school-leave",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-software-engineer-job",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-sports-scholarship",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-teacher-job",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-transfer-certificate-request",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-   {
+    {
       url: "https://aavedanpatra.in/applications/application-for-university-admission",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-       {
+    {
       url: "https://aavedanpatra.in/applications/application-for-urgent-leave",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-      {
+    {
       url: "https://aavedanpatra.in/applications/application-for-vacation-leave",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
-    ,
     {
       url: "https://aavedanpatra.in/about",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/faq",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/support",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/privacy-policy",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/terms-of-service",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/disclaimer",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
     {
       url: "https://aavedanpatra.in/contact-us",
-      lastModified: new Date(),
+      lastModified: "2024-12-01",
     },
   ];
 }
