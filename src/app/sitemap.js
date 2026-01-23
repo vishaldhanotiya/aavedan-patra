34ece6dd75455d6ab60edd30b48893729ebb2c5c
Vishal Dhanotiya <vishal.dhanotiya2@gmail.com>
2026-01-23 23:20:52 +0530

    added more url in sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,135 +2,151 @@ export default function sitemap() {
   return [
     {
       url: "https://aavedanpatra.in",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/job",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
+    },
+    {
+      url: "https://aavedanpatra.in/applications/loan",
+      lastModified: "2025-12-01",
+    },
+    {
+      url: "https://aavedanpatra.in/applications/admission",
+      lastModified: "2025-12-01",
+    },
+    {
+      url: "https://aavedanpatra.in/applications/scholarship",
+      lastModified: "2025-12-01",
+    },
+    {
+      url: "https://aavedanpatra.in/applications/certificate-request",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-bank-job",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-bonafide-certificate-request",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-business-loan",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-car-loan",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-caste-certificate-request",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-casual-leave",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-character-certificate-request",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-college-admission",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-course-admission",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-education-loan",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-emergency-loan",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-experience-certificate-request",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-government-scholarship",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-home-loan",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-income-certificate",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-manager-position-job",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-merit-scholarship",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-need-based-scholarship",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-office-assistant-job",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-personal-leave",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-personal-loan",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-private-scholarship",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-readmission",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-research-grant",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-school-admission",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-school-leave",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-software-engineer-job",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-sports-scholarship",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-teacher-job",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-transfer-certificate-request",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/applications/application-for-university-admission",
@@ -144,33 +160,150 @@ export default function sitemap() {
       url: "https://aavedanpatra.in/applications/application-for-vacation-leave",
       lastModified: "2024-12-01",
     },
+
+    {
+      url: "https://aavedanpatra.in/letters",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/request",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/complaint",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/resignation",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/invitation",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/birthday-party-invitation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/business-meeting-invitation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/career-change-resignation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/donation-request-letter",
+      lastModified: "2026-01-02",
+    },
+
+    {
+      url: "https://aavedanpatra.in/letters/event-invitation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/guest-speaker-invitation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/immediate-resignation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/information-request-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/meeting-request-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/neighbor-complaint-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/online-shopping-complaint-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/permission-request-letter",
+      lastModified: "2026-01-02",
+    },
+
+    {
+      url: "https://aavedanpatra.in/letters/product-complaint-letter",
+      lastModified: "2026-01-02",
+    },
+
+    {
+      url: "https://aavedanpatra.in/letters/professional-resignation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/recommendation-request-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/refund-request-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/relocation-resignation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/retirement-resignation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/seminar-invitation-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/service-complaint-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/short-notice-resignation-letter",
+      lastModified: "2026-01-02",
+    },
+
+    {
+      url: "https://aavedanpatra.in/letters/sponsorship-request-letter",
+      lastModified: "2026-01-02",
+    },
+    {
+      url: "https://aavedanpatra.in/letters/workplace-complaint-letter",
+      lastModified: "2026-01-02",
+    },
     {
       url: "https://aavedanpatra.in/about",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/faq",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/support",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/privacy-policy",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/terms-of-service",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/disclaimer",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
     {
       url: "https://aavedanpatra.in/contact-us",
-      lastModified: "2024-12-01",
+      lastModified: "2025-12-01",
     },
   ];
 }
