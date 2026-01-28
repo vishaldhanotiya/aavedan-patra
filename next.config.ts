e1b13d6894b31e093de25d3a09ee6c0f4f1ba14a
Vishal Dhanotiya <vishal.dhanotiya2@gmail.com>
2026-01-29 01:05:18 +0530

    improve next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
