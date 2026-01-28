f227081cd2389f24c5cd606e24fff3477db66fde
Vishal Dhanotiya <vishal.dhanotiya2@gmail.com>
2026-01-29 00:18:33 +0530

    update template and change some words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-violet-50 dark:from-slate-950 dark:via-blue-950 dark:to-violet-950">
-      <LanguageSwitcher />
+      {/* <LanguageSwitcher /> */}
       <Hero />
       <CategoryGrid />
       <TrendingTemplates />
