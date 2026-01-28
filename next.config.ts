9a9238c91e441fc44000713bdf2c1e57c4fa07fc
Vishal Dhanotiya <vishal.dhanotiya2@gmail.com>
2026-01-29 01:14:09 +0530

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -14,8 +14,16 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
     ];
   },
 };
-
 export default nextConfig;
